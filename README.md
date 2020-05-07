115cebf13881ef7264dedc19b44b6858fbeaa0d3
Symon Cristhian <contatosymon@gmail.com>
2020-05-07 11:42:14 -0400

    Front-End

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Foodfy
 Foodfy is a web site about recipes
+
+Foodfy's Front-End uses HTML 5, CSS 3 and Javascript.
---
+++ public/js/script.js
@@ -0,0 +1,23 @@
+const modalOverlay = document.querySelector('.modalOverlay')
+const cartoes = document.querySelectorAll('.cartao')
+
+for (let cartao of cartoes) {
+    cartao.addEventListener("click", function () {
+        const receitaId = cartao.getAttribute("id")
+        const nome = cartao.querySelector("#nome").textContent
+        const autor = cartao.querySelector("#autor").textContent
+        modalOverlay.classList.add('active')
+        modalOverlay.querySelector('img').src = `public/img/${receitaId}.png`
+        modalOverlay.querySelector('.autorModal').innerHTML = `${autor}`
+        modalOverlay.querySelector('.nomeModal').innerHTML = `${nome}`
+
+    })
+}
+
+
+
+
+
+document.querySelector("#closeModal").addEventListener("click", function () {
+    modalOverlay.classList.remove('active')
+})
